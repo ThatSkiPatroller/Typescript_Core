27948a495c8c2f0fb8c0bcc1de5c3df1efa751e8
ThatSkiPatroller <69833928+ThatSkiPatroller@users.noreply.github.com>
2021-11-03 03:34:45 -0500

    Added notes and ts file for interfaces section

---
+++ Interfaces/interfaces.ts
@@ -0,0 +1,39 @@
+// always use capital letter for interfaces
+interface Reportable {
+    summary(): string;
+}
+// interface Vehicle {
+//     name: string;
+//     year: Date;
+//     broken: boolean;
+//     summary(): string;
+// }
+
+
+const oldCivic = {
+    name: 'civic',
+    year: new Date(),
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`;
+    }
+};
+
+const food = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`;
+    }
+};
+
+const printSummary = (item: Reportable): void => {
+    console.log(item.summary);
+    // console.log(`Name: ${vehicle.name}`);
+    // console.log (`Year: ${vehicle.year}`);
+    // console.log(`Broken? ${vehicle.broken}`);
+};
+
+printSummary(oldCivic);
+printSummary(food);
